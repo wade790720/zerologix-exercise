5f36faee3b694a2ac575b49393bee9e9d4e9615a
wadezhu <wade790720@gmail.com>
2022-08-08 01:41:31 +0800

    Add data volume increase and decrease function

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import StockChart from "components/StockChart";
 import mockData from "mock/buckets.json";
 import { ReactComponent as ZoomIn } from "assets/zoom-In.svg";
 import { ReactComponent as ZoomOut } from "assets/zoom-out.svg";
+import { Plus, Minus } from "tabler-icons-react"
 
 function App() {
-  const buckets = Array.from(mockData.values()).slice(420);  // 設0為500筆資料
   const [scale, setScale] = useState(1)
+  const [buckets, setBuckets] = useState(Array.from(mockData.values()).slice(420)) // 設0為500筆資料
   const scaleMultiplier = 0.8;
 
   const handleZoomIn = () => {
@@ -19,6 +20,16 @@ function App() {
     setScale(scale * scaleMultiplier)
   };
 
+  const handleAddData = () => {
+    if (mockData.length - buckets.length - 20 < 0) return
+    setBuckets(Array.from(mockData.values()).slice(mockData.length - buckets.length - 20))
+  }
+
+  const handleDeleteData = () => {
+    if(mockData.length - buckets.length + 20 >= mockData.length) return
+    setBuckets(Array.from(mockData.values()).slice(mockData.length - buckets.length + 20))
+  }
+
   return (
     <div className={style.container}>
       <div className={style.inner}>
@@ -28,6 +39,12 @@ function App() {
           </Toolbar.Item>
           <Toolbar.Item onClick={handleZoomOut}>
             <ZoomOut />
+          </Toolbar.Item>
+          <Toolbar.Item onClick={handleAddData}>
+            <Plus />
+          </Toolbar.Item>
+          <Toolbar.Item onClick={handleDeleteData}>
+            <Minus />
           </Toolbar.Item>
         </Toolbar>
         <div className={style.chart}>
